8f671b6fdd43c0e868daff6c2a2551489c5cff17
abe <zeamanualfeleke@gmail.com>
2022-07-05 14:55:37 +0300

    contents of service section updated along with some styling

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -15,20 +15,20 @@ function Service() {
             <div className='service'>
                 <img src={webDev}  alt='web'></img>
                 <h2 className='titile'>Website Development</h2>
-                <p>information about servie information about servie information about servieinformation about servie information about servieinformation about servie </p>
+                <p>We will help you create all types of web-based software and ensure great experience for web users. At Z-Tech,  we professionally design, redesign and continuously support customer-facing and enterprise web apps to achieve high conversion and adoption rates.</p>
                 <FontAwesomeIcon className='arrow' icon={faArrowRight}></FontAwesomeIcon>
             </div>
             <div className='service'>
                 <img src={mobileDev}  alt='mobile app'></img>
                 <h2 className='titile'>Mobile App Development</h2>
-                <p>information about servie information about servie information about servieinformation about servie information about servieinformation about servie </p>
+                <p>If you’re looking to leverage mobile technology for your business, our best app developers are at your service. We provide iOS and Android mobile application development services so you can reach your customers on their favorite devices.</p>
                 <FontAwesomeIcon className='arrow' icon={faArrowRight}></FontAwesomeIcon>
                 
             </div>
             <div className='service'>
                 <img src={eComm}  alt='ecommerice'></img>
                 <h2 className='titile'>E-Commerice</h2>
-                <p>information about servie information about servie information about servieinformation about servie information about servieinformation about servie </p>
+                <p>We will supply businesses with application tools designed to help them sell their services and products online. We provide a range of secure services to their clients, which include everything from product catalog management to online payment processing. </p>
                 <FontAwesomeIcon className='arrow' icon={faArrowRight}></FontAwesomeIcon>
             </div>
         </div>
