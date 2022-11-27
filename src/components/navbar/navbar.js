2d7c4e07a3a52d99ea72d50bf84cefd0fe7a4be3
abe <zeamanualfeleke@gmail.com>
2022-11-27 11:35:08 +0300

    navbar items responsiveness updated

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,34 +1,37 @@
 import React from 'react'
 import './navbar.css'
-import {useState} from 'react'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import logo from '../../assets/image/logo.png'
 function NavBar() {
-    let [navShown,setNavShown]=useState(false)
-    let handleNavBtn=()=>{
-        setNavShown((prevState)=>{
+    let [navShown, setNavShown] = useState(false)
+    let handleNavBtn = () => {
+        setNavShown((prevState) => {
             return !prevState
         })
     }
-  return (
-    <div>
-        <nav>
-            <FontAwesomeIcon className='nav-btn' icon={faBars} onClick={handleNavBtn}  />
-            <a href='#header'> <img src={logo} alt='company logo' className="logo"/></a>
-            <div className= {navShown ? 'nav-lists show-nav-bar' :'nav-lists'}>
-                <ul>
-                    <li><a href='#service'> Services  </a></li>
-                    <li><a href='#about'>  About </a></li>
-                    <li><a href='#teams'>  Teams </a></li> 
-                    <li><a href='#testimonial'>  Client FeedBack </a></li>
-                    <a className="contact-us-btn" href='#contact'>Contact Us </a>
-                </ul>
-                
-            </div>
-        </nav>
-    </div>
-  )
+    let handleNavItemsClinkOnSmallScreen = () => {
+        setNavShown(false)
+    }
+    return (
+        <div>
+            <nav>
+                <FontAwesomeIcon className='nav-btn' icon={faBars} onClick={handleNavBtn} />
+                <a href='#header'> <img src={logo} alt='company logo' className="logo" /></a>
+                <div className={navShown ? 'nav-lists show-nav-bar' : 'nav-lists'}>
+                    <ul>
+                        <li onClick={handleNavItemsClinkOnSmallScreen}><a href='#service'> Services  </a></li>
+                        <li onClick={handleNavItemsClinkOnSmallScreen}><a href='#about'>  About </a></li>
+                        <li onClick={handleNavItemsClinkOnSmallScreen}><a href='#teams'>  Teams </a></li>
+                        <li onClick={handleNavItemsClinkOnSmallScreen}><a href='#testimonial'>  Client FeedBack </a></li>
+                        <a onClick={handleNavItemsClinkOnSmallScreen} className="contact-us-btn" href='#contact'>Contact Us </a>
+                    </ul>
+
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default NavBar
